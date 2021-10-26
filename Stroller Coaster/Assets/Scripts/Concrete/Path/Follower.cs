10bda7f29cedc48d171031eba6adaf0d28353439
gulersuleyman <ggulersuleyman@gmail.com>
2021-10-26 13:02:53 +0300

    Add path follower

---
+++ Stroller Coaster/Assets/Scripts/Concrete/Path/Follower.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PathCreation;
+
+public class Follower : MonoBehaviour
+{
+    [SerializeField] PathCreator _pathCreator;
+    [SerializeField] float _speed;
+
+    float _distanceTravelled;
+    
+    void Update()
+    {
+        _distanceTravelled += _speed * Time.deltaTime;
+        transform.position = _pathCreator.path.GetPointAtDistance(_distanceTravelled);
+        transform.rotation = _pathCreator.path.GetRotationAtDistance(_distanceTravelled);
+    }
+}
